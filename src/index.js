e503169c86330f0317ad1f3ef3b8cd0216b7b402
Vinay Pandya <005vinay@gmail.com>
2018-08-24 15:47:21 +0530

    redux setup

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
-import Routes from './Routes';
 import './styles/styles.scss';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './rootReducer';
+import Routes from './Routes';
+
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 const jsx = (
-  <BrowserRouter>
-    <div>
-      {renderRoutes(Routes)}
-    </div>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        {renderRoutes(Routes)}
+      </div>
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
